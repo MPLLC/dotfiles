29ad98e5b01538b6e44f19af8349094448eaddfc
Kevin Major <kevinmajor@mac.lan>
2025-09-22 12:46:01 -0400

    lsp deprecation fix
    Updated all lsp entries to use the non-deprecated vim.lsp.enable()
    syntax

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,59 +1,58 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"denols",
-					"clangd",
-					"tailwindcss",
-					"dockerls",
-					"gopls",
-					"html",
-					"ts_ls",
-					"jsonls",
-					"marksman",
-					"phpactor",
-					"rust_analyzer",
-					"svelte",
-					"taplo",
-					"twiggy_language_server",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "denols",
+          "clangd",
+          "tailwindcss",
+          "dockerls",
+          "gopls",
+          "html",
+          "ts_ls",
+          "jsonls",
+          "marksman",
+          "phpactor",
+          "rust_analyzer",
+          "svelte",
+          "taplo",
+          "twiggy_language_server",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.denols.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.tailwindcss.setup({ capabilities = capabilities })
-			lspconfig.dockerls.setup({ capabilities = capabilities })
-			lspconfig.gopls.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-			lspconfig.jsonls.setup({ capabilities = capabilities })
-			lspconfig.marksman.setup({ capabilities = capabilities })
-			lspconfig.phpactor.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-			lspconfig.svelte.setup({ capabilities = capabilities })
-			lspconfig.taplo.setup({ capabilities = capabilities })
-			lspconfig.twiggy_language_server.setup({ capabilities = capabilities })
+      vim.lsp.enable("lua", { capabilities = capabilities })
+      vim.lsp.enable("denols", { capabilities = capabilities })
+      vim.lsp.enable("clangd", { capabilities = capabilities })
+      vim.lsp.enable("tailwindcss", { capabilities = capabilities })
+      vim.lsp.enable("dockerls", { capabilities = capabilities })
+      vim.lsp.enable("gopls", { capabilities = capabilities })
+      vim.lsp.enable("html", { capabilities = capabilities })
+      vim.lsp.enable("ts_ls", { capabilities = capabilities })
+      vim.lsp.enable("jsonls", { capabilities = capabilities })
+      vim.lsp.enable("marksman", { capabilities = capabilities })
+      vim.lsp.enable("phpactor", { capabilities = capabilities })
+      vim.lsp.enable("rust_analyzer", { capabilities = capabilities })
+      vim.lsp.enable("svelte", { capabilities = capabilities })
+      vim.lsp.enable("taplo", { capabilities = capabilities })
+      vim.lsp.enable("twiggy_language_server", { capabilities = capabilities })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>def", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>def", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
